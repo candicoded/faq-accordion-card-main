c8312978dde4f34ef8365f0e4eb2714e95718339
candicoded <103226511+candicoded@users.noreply.github.com>
2023-09-01 11:47:16 -0400

    added styles and js

---
+++ script.js
@@ -0,0 +1,13 @@
+let question = document.querySelectorAll('.question');
+
+for (let i = 0; i < question.length; i++) {
+  question[i].addEventListener("click", function() {
+    
+    let answer = this.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+}
