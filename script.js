5fe101aa1fb2d0d34bbe92b064d70ebaf7b2355e
candicoded <103226511+candicoded@users.noreply.github.com>
2023-09-04 19:04:57 -0400

    styled box and fixed accordion

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-// let question = document.querySelectorAll('.question');
+let question = document.querySelectorAll('.question');
 
-// for (let i = 0; i < question.length; i++) {
-//   question[i].addEventListener("click", function() {
+for (let i = 0; i < question.length; i++) {
+  question[i].addEventListener("click", function() {
     
-//     let answer = this.nextElementSibling;
-//     if (answer.style.display === "block") {
-//       answer.style.display = "none";
-//     } else {
-//       answer.style.display = "block";
-//     }
-//   });
-// }
+    let answer = this.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+      question[i].classList.remove('active-question');
+    } else {
+      answer.style.display = "block";
+      question[i].classList.add('active-question');
+    }
+  });
+}
