e17cc3e485f82b91ddf8829b16b4254bedc5edad
candicoded <103226511+candicoded@users.noreply.github.com>
2023-09-04 21:51:39 -0400

    fixed rotate on arrow

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let question = document.querySelectorAll('.question');
+let arrow = document.styleSheets[0].cssRules[9];
 
 for (let i = 0; i < question.length; i++) {
   question[i].addEventListener("click", function() {
@@ -7,9 +8,11 @@ for (let i = 0; i < question.length; i++) {
     if (answer.style.display === "block") {
       answer.style.display = "none";
       question[i].classList.remove('active-question');
+      arrow.style.transform = "rotate(0deg)";
     } else {
       answer.style.display = "block";
       question[i].classList.add('active-question');
+      arrow.style.transform = "rotate(180deg)";
     }
   });
 }
